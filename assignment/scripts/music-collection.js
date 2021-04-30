6b908b960978bf3a1f0a5f27198f3b5d03be5d39
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-04-29 21:29:06 -0500

    added 6 albums to collection

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -18,6 +18,22 @@ function addToCollection( title, artist, yearPublished ) {
   return album; //end function
 }
 
-addToCollection( 'Californian Soil', 'London Grammar', 2021 );
+// - Test the `addToCollection` function:
+//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//   - Console.log each album as added using the returned value.
+//   - After all are added, console.log the `collection` array.
 
+console.log( addToCollection( 'Californian Soil', 'London Grammar', 2021 ) );
+
+console.log( addToCollection( 'dont smile at me', 'Billie Eilish', 2017 ) );
+
+console.log( addToCollection( 'Sing To Me Instead', 'Ben Platt', 2019 ) );
+
+console.log( addToCollection( 'WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019 ) );
+
+console.log( addToCollection( 'In The Lonely Hour', 'Sam Smith', 2014 ) );
+
+console.log( addToCollection( 'Days Are Gone', 'HAIM', 2013 ) );
+
+console.log(`Check to make sure the albums were added to my collection:`);
 console.log( collection );
